d9b136bf697a24aa2b551060df32b2078908a93d
samskproduction <samskworld6@gmail.com>
2024-09-30 16:53:25 +0500

    message (meaningfull)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import profile from '../img.jpeg'
+import img from '../img.jpeg'
 import Image from "next/image";
 
 const page = () => {
@@ -7,14 +7,16 @@ const page = () => {
     <div>
     <section className=" min-height[100vh] py-1.5 pr-[12%] flex items-center justify-center" >
     <div className="flex flex-col mt-40 ">
-        <h1 className='text-5xl text-black'>Hi, Its me <span className='text-aqua text-5xl '> <strong>Muhammad Sameer</strong></span></h1>
-        <p className='max-w-screen-lg  text-3xl px-2.5 py-2.5 mt-20 text-black'>
-        I m a frontend developer, passionate about technology and creating innovative solutions.<br/> I have a strong foundation in programming languages such as Html,Css,and JavaScript,<br/> I m currently working on Nextjs projects, and I m always eager to learn and grow.
+        <h1 className='text-5xl text-black text-bold'>Hi, Its me <span className='text-[#569DAA] text-6xl text-extrabold '> <strong>Muhammad Sameer</strong></span></h1>
+        <p className='max-w-screen-lg  text-4xl px-2 py-2 mt-20 text-black'>
+        I m a frontend developer, passionate about technology and creating innovative solutions.
+        I have a strong foundation in programming languages such as Html,Css,and JavaScript, I m currently working on Nextjs projects, and I m always eager to learn and grow.
         </p>
-      
     </div>
     <div className=''>
-<Image className ="relative top-12 w-[32vw] rounded-full h-[32vw] object-cover mt-44 left-56 cursor-pointer ease-in-out[0.4s]]"src={profile} alt='profile' />
+<Image className ="relative top-12 w-[32vw] rounded-full h-[32vw] object-cover mt-44 left-56 cursor-pointer ease-in-out[0.4s]]"
+src={img} 
+alt='profile' />
 </div>
     </section>
     </div>
