cd45c9e91f91450fdb2b166c7f7af8a5f564a12f
samskproduction <163208789+samskproduction@users.noreply.github.com>
2024-10-03 14:51:12 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,8 @@ const page = () => {
     <section className=" min-height[100vh] py-1.5 pr-[12%] flex items-center justify-center" >
     <div className="flex flex-col mt-40 ">
         <h1 className='text-5xl text-black text-bold'>Hi, Its me <span className='text-[#569DAA] text-6xl text-extrabold '> <strong>Muhammad Sameer</strong></span></h1>
-        <p className='max-w-screen-lg  text-4xl px-2 py-2 mt-20 text-black'>
+        <p className='max-w-screen-lg  text-4xl px-2.5 py-2.5 mt-20 text-black'>
         I m a frontend developer, passionate about technology and creating innovative solutions.
-        I have a strong foundation in programming languages such as Html,Css,and JavaScript, I m currently working on Nextjs projects, and I m always eager to learn and grow.
         </p>
     </div>
     <div className=''>
